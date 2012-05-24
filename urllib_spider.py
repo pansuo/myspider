c2ef432d1a067987628ec49a7b81103d6c5fface
sgyhunter <sgyhunter@126.com>
2012-05-24 22:08:49 +0800

    previous version will fail on 阿祖拉 as the keyword: experiment show that: raw input=>kw=>gbk=>encode2url=>search,                             |=>URL then the search will failed, but: raw input=>kw=>gbk=>encode2url=>search,             |=>utf-8->encode2url=>URL will successful while 毛泽东 as key word does not behave like above,it just work normally in previouse version code real root cause don't know, just find the trick of it

--- urllib_spider.py
+++ urllib_spider.py
@@ -20,13 +20,21 @@ else:
 
 #keyword must be encoded into 
 #gbk or gb18030
-kw_gbk = keyword.encode('gb18030') 
-m = {'wd':kw_gbk,}
-keyword_url = urllib.urlencode(m)
+kw_gbk = keyword.encode('gbk') 
+kw_utf8 = keyword.encode('utf-8')
+m1 = {'wd':kw_gbk}
+m2 = {'wd':kw_utf8}
+
+keyword_url1 = urllib.urlencode(m1)
+keyword_url2 = urllib.urlencode(m2)
+print 'keyword_url from gbk is:'+keyword_url1
+print 'keyword_url from utf8 is:'+keyword_url2
+
+
 header_page0 = {
  'User-Agent':'Mozilla/5.0 (Windows NT 6.1; rv:12.0) Gecko/20100101 Firefox/12.0',
  'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
- 'Accept-Language':'zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3',
+# 'Accept-Language':'zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3',
 # 'Accept-Encoding':'gzip, deflate',
  'DNT':'1',
  'Connection':'keep-alive',
@@ -34,9 +42,10 @@ header_page0 = {
  'Cookie':'BAIDUID=79741D75BB0E5D383F2D6FF9AE0827B6:FG=1; BDUT=rv3679741D75BB0E5D383F2D6FF9AE0827B613686caba690; BDREFER=%7Burl%3A%22http%3A//news.baidu.com/%22%2Cword%3A%22%22%7D; BDRCVFR[eHt_ClL0b_s]=mk3SLVN4HKm'
 	}
 
+
 def check_nextPage(content,page):
-	str1 = '<a href="s?%s&pn=%d&usm=3">%d</a>' %(keyword_url,page+10,(page/10)+2)
-#	print 'search for:%s' % str1
+	str1 = '<a href="s?%s&pn=%d">%d</a>' %(keyword_url1,page+10,(page/10)+2)
+	print 'search string:%s' % str1
 	if content.find(str1) == -1:
 		print 'page%d is not exist'%(page/10+2)
 		return False
@@ -49,7 +58,7 @@ if __name__ == '__main__':
 #	curUrl = 'http://www.baidu.com/s?wd=%s&rsv_bp=0&rsv_spt=3&inputT=%d' %(keyword,inputTime)
 #	Yes! must use keyword_url in below curUrl or urllib2.urlopen will report 
 #	UnicodeEncodError
-	curUrl = 'http://www.baidu.com/s?%s&rsv_bp=0&rsv_spt=3&inputT=%d' %(keyword_url,inputTime) 
+	curUrl = 'http://www.baidu.com/s?%s&rsv_bp=0&rsv_spt=3&inputT=%d' %(keyword_url2,inputTime) 
 	req = urllib2.Request(url=curUrl,data=None,headers=header_page0)
 #get the first page 
 	content = urllib2.urlopen(req).read()
@@ -63,14 +72,14 @@ if __name__ == '__main__':
 		header_pageX = {
 		 'User-Agent':'Mozilla/5.0 (Windows NT 6.1; rv:12.0) Gecko/20100101 Firefox/12.0',
 		 'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-		 'Accept-Language':'zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3',
+#		 'Accept-Language':'zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3',
 #		 'Accept-Encoding':'gzip, deflate',
 		 'DNT':'1',
 		 'Connection':'keep-alive',
 		 'Referer':lastUrl,
 		 'Cookie':'BAIDUID=79741D75BB0E5D383F2D6FF9AE0827B6:FG=1; BDUT=rv3679741D75BB0E5D383F2D6FF9AE0827B613686caba690; BDREFER=%7Burl%3A%22http%3A//news.baidu.com/%22%2Cword%3A%22%22%7D; BDRCVFR[eHt_ClL0b_s]=mk3SLVN4HKm'
 		}
-		curUrl = 'http://www.baidu.com/s?%s&pn=%d&usm=3' %(keyword_url,curPage) #&usm=3
+		curUrl = 'http://www.baidu.com/s?%s&pn=%d' %(keyword_url2,curPage) #&usm=3
 		req = urllib2.Request(url=curUrl,data=None,headers=header_pageX)
 		content = urllib2.urlopen(req).read()
 
